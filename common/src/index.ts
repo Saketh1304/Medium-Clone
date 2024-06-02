7857e9773686fa66f3968111d496c9252a8a2e29
Your Name <you@example.com>
2024-06-02 20:46:45 +0530

    Updated

---
+++ common/src/index.ts
@@ -0,0 +1,31 @@
+import z from "zod";
+
+export const signupInput = z.object({
+    username: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().optional(),
+});
+
+export type SignupType = z.infer<typeof signupInput>;
+
+export const signinInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+});
+
+export type SigninType = z.infer<typeof signinInput>;
+
+export const createBlogInput = z.object({
+    title: z.string(),
+    content: z.string(),
+});
+
+export type CreateBlogType = z.infer<typeof createBlogInput>;
+
+export const updateBlogInput = z.object({
+    title: z.string(),
+    content: z.string(),
+    id:z.number()
+});
+
+export type UpdateBlogType = z.infer<typeof updateBlogInput>;
